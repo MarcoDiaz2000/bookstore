81a53614a1847590d0785c9e99ab9cc9a0c1408f
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-08 07:06:14 -0600

    Add navigation component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Route, Link, Routes,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavigationComponent from './components/NavigationComponent';
 import Books from './router/Books';
 import Categories from './router/Categories';
-import logo from './images/logo.svg';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <div className="navbar">
-        <Link to="/">
-          <img src={logo} className="logo" alt="logo" />
-        </Link>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/" className="nav-link">Books</Link>
-            </li>
-            <li>
-              <Link to="/categories" className="nav-link">Categories</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <NavigationComponent />
 
       <Routes>
         <Route path="/" element={<Books />} />
-        <Route path="categories" element={<Categories />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="*" element={<div>If page not found it goes here</div>} />
       </Routes>
     </Router>
