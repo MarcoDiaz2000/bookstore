315ddb2f5a8361f8c429fcbba544d17bf790318e
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-12 10:34:10 -0600

    Add Store

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState: [],
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload.id),
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
