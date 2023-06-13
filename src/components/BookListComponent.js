a2355a564f3d1fb02516e9b51a86bfd96cd228d7
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-13 05:15:22 -0600

    Add Redux in components

--- src/components/BookListComponent.js
+++ src/components/BookListComponent.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import BookComponent from './BookComponent';
 
-const BookList = ({ books, handleDelete }) => (
-  <div>
-    {books.map((book) => <BookComponent key={book.id} book={book} handleDelete={handleDelete} />)}
-  </div>
-);
+const BookList = ({ handleDelete }) => {
+  const books = useSelector((state) => state.books);
+
+  return (
+    <div>
+      {books.map((book) => <BookComponent key={book.id} book={book} handleDelete={handleDelete} />)}
+    </div>
+  );
+};
 
 BookList.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
   handleDelete: PropTypes.func.isRequired,
 };
 
