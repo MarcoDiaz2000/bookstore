d5b8e7d98e55f08766192ed70cacfa4fd79f5ffd
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-14 09:43:55 -0600

    Add Boostrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
